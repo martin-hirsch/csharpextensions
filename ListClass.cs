b96cc98cac2547fa4c98748ee116451d59690b1a
martin-hirsch <60873837+martin-hirsch@users.noreply.github.com>
2020-02-10 16:26:39 +0100

    Update ListClass.cs

--- ListClass.cs
+++ ListClass.cs
@@ -5,6 +5,15 @@ public abstract class ListClass<T>
         Items = items.ToList();
     }
 
+    protected ListClass()
+    {
+        Items = new List<T>();
+    }
+
+    public int Count => Items.Count;
+
+    public IList<T> Items { get; protected set; }
+
     public void AddRange(IEnumerable<T> other)
     {
         foreach (var item in other)
@@ -12,12 +21,4 @@ public abstract class ListClass<T>
             Items.Add(item);
         }
     }
-    
-    protected ListClass()
-    {
-        Items = new List<T>();
-    }
-
-    public int Count => Items.Count;
-    public IList<T> Items { get; protected set; }
 }
