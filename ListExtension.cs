14c83ad234bc104abea7e8eab150ec0e1ab11ca4
martin-hirsch <60873837+martin-hirsch@users.noreply.github.com>
2020-06-17 16:54:07 +0200

    Create ListExtension.cs

---
+++ ListExtension.cs
@@ -0,0 +1,27 @@
+public static (List<T> firstHalf, List<T> secondHalf, bool divisionByTwoIsPossible) HalfList<T>(this List<T> me)
+        {
+            var firstHalf = new List<T>();
+            var secondHalf = new List<T>();
+            var divisionByTwoIsPossible = false;
+            if (me.Count == 1)
+            {
+                firstHalf.Add(me.Single());
+            }
+            else if (me.Count % 2 != 0)
+            {
+                var r = me.Count / 2d;
+                var r2 = (int)Math.Round(r, MidpointRounding.ToEven);
+                firstHalf.AddRange(me.GetRange(0, r2));
+                secondHalf.AddRange(me.Where(x => !firstHalf.Contains(x)));
+            }
+            else
+            {
+                var halfSize = me.Count / 2;
+                firstHalf.AddRange(me.GetRange(0, halfSize));
+                secondHalf.AddRange(me.Where(x => !firstHalf.Contains(x)));
+
+                divisionByTwoIsPossible = true;
+              }
+            
+  return (firstHalf, secondHalf, divisionByTwoIsPossible);
+}
