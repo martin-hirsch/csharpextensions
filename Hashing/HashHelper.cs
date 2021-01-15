26a02fed8a675719c0ccff0e30e5ab9c3d2d1f95
martin-hirsch <60873837+martin-hirsch@users.noreply.github.com>
2021-01-15 07:39:44 +0100

    Create HashHelper.cs

---
+++ Hashing/HashHelper.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+    public static class HashHelper
+    {
+        public static int GetSequenceHash<TItem>(this IEnumerable<TItem> other)
+        {
+            const int B = 378551;
+            var a = 63689;
+            var hash = 0;
+
+            if (other == null)
+            {
+                return hash;
+            }
+            
+            unchecked // If it overflows then just wrap around
+            {
+                foreach (var obj in other)
+                {
+                    if (obj == null)
+                    {
+                        continue;
+                    }
+
+                    hash = hash * a + obj.GetHashCode();
+                    a *= B;
+                }
+            }
+
+            return hash;
+        }
+    }
